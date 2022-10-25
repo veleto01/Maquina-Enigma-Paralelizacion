957cf677ee493abfb489f962044b49257bdb4b72
Manuel Alejandro <31254691+veleto01@users.noreply.github.com>
2022-10-25 21:38:29 +0200

    Maquina Enigma
    Primera práctica de Computación de altas prestaciones.
    Curso 2021-2022.

---
+++ lab02.cpp
@@ -0,0 +1,29 @@
+#include <mpi.h>
+#include <stdio.h>
+int main(int argc, char** argv) {
+	const int MAX_NUMBERS = 100;
+	int numbers[MAX_NUMBERS];
+	int number_amount;
+	if (world_rank == 0) {
+		// Pick a random amount of integers to send to process one
+		srand(time(NULL));
+		number_amount = (rand() / (float)RAND_MAX) * MAX_NUMBERS;
+		// Send the amount of integers to process one
+		MPI_Send(numbers, number_amount, MPI_INT, 1, 0, MPI_COMM_WORLD);
+		printf("0 sent %d numbers to 1\n", number_amount);
+	}
+	else if (world_rank == 1) {
+		MPI_Status status;
+		// Receive at most MAX_NUMBERS from process zero
+		MPI_Recv(numbers, MAX_NUMBERS, MPI_INT, 0, 0, MPI_COMM_WORLD,
+			&status);
+		// After receiving the message, check the status to determine
+		// how many numbers were actually received
+		MPI_Get_count(&status, MPI_INT, &number_amount);
+		// Print off the amount of numbers, and also print additional
+		// information in the status object
+		printf("1 received %d numbers from 0. Message source = %d, "
+			"tag = %d\n",
+			number_amount, status.MPI_SOURCE, status.MPI_TAG);
+	}
+}
